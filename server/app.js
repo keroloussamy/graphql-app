4ec4f857018db9353a26dac9a4e990b99492419a
keroloussamy <keroloussamygrace@gmail.com>
2022-10-13 20:21:31 +0200

    Root Query

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 //express don't know how to deal with graphql but express-graphql. So any request for graphql let express-graphql deal with it.
 app.use('/qraphql', graphqlHTTP({
-
+    schema
 }))
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,3 +9,23 @@ const BookType = new GraphQLObjectType({
     genre: { type: GraphQLString },
   })
 })
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {id: { type: GraphQLString }},
+      resolve(parent, args){ 
+        // Code to get data from DB.
+      }
+    }
+  }
+});
+
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
+
