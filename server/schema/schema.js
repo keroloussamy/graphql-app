9d45f0f2549408727bd30b43b9275300d8755f8a
keroloussamy <keroloussamygrace@gmail.com>
2022-10-14 19:43:01 +0200

    Add Author Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,16 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
-const data = [
+const bookData = [
   {id: 1, name: 'John Doe', genre: 'Book 1'},
   {id: 2, name: 'Emilia William', genre: 'Book 2'},
   {id: 3, name: 'Tito Well', genre: 'Book 3'}
+]
+
+const authorData = [
+  {id: 1, name: 'Author 1', age: 1},
+  {id: 2, name: 'Author 2', age: 2},
+  {id: 3, name: 'Author 3', age: 3}
 ]
 
 
@@ -17,6 +23,16 @@ const BookType = new GraphQLObjectType({
   })
 })
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  })
+})
+
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -24,8 +40,19 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: {id: { type: GraphQLID }},
       resolve(parent, args){ 
+        /* 
+          Note there's no ID type on JS, So GraphQLID is converted to String on JS.
+          So e.id === args.id is not equal because this int and that is string now.
+        */
+        return bookData.find((e) => e.id == args.id );
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {id: { type: GraphQLID }},
+      resolve(parent, args){ 
         // Get data from DB.
-        return data.find((e) => e.id === args.id );
+        return authorData.find((e) => e.id == args.id );
       }
     }
   }
