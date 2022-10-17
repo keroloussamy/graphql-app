cab5d4b760c0738e7c2aefe1decbe36957a966f7
keroloussamy <keroloussamygrace@gmail.com>
2022-10-17 02:07:09 +0200

    Connect to MongoDb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const MONGO_URL = 'mongodb+srv://kero:1234@cluster0.2eous.mongodb.net/graphql-app?retryWrites=true&w=majority'
+mongoose.connect(MONGO_URL)
+mongoose.connection.once('open', () => {
+    console.log('Connected to database');
+});
+
 
 //express don't know how to deal with graphql but express-graphql. So any request for graphql let express-graphql deal with it.
 app.use('/graphql', graphqlHTTP({
